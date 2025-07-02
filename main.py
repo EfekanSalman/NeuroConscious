9814f8b26516699ed022cd39bc0e0cb4526c9de9
efekan <efekansalman@gmail.com>
2025-07-02 19:22:14 +0300

    feat: added plotter.py the code visually tracks agent behavior with matplotlib. Mood information is reflected in the graph in color

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from agent.base_agent import Agent
+# main.py
+
 from core.mood.basic_mood import BasicMoodStrategy
+from agent.base_agent import Agent
 from environment.world import World
+from visualization.plotter import AgentLogger
 
-
-def run_simulation(steps = 10):
+def main():
     mood_strategy = BasicMoodStrategy()
-    agent = Agent(name = "SimBot", mood_strategy = mood_strategy)
+    agent = Agent(name="SimBot", mood_strategy=mood_strategy)
+    logger = AgentLogger(agent_name=agent.name)
 
     world = World()
     world.add_agent(agent)
 
-    world.run(steps = 15)
+    for step in range(20):
+        world.update_environment()
+        agent.sense()
+        action = agent.think()
+        agent.act(action)
+
+        logger.log(step, agent.state.hunger, agent.state.fatigue, agent.state.mood)
+
+    logger.plot()
 
 if __name__ == "__main__":
-    run_simulation()
+    main()
